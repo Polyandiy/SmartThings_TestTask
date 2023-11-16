2ee06f344b76fa66989e5e6136e5df5e1c8da1bd
Polina Skuratova <skuratova250496@gmail.com>
2023-11-16 16:42:23 +0700

    Initial Commit

---
+++ SmartThings_TestTask/SmartThings_TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartThings_TestTaskApp.swift
+//  SmartThings_TestTask
+//
+//  Created by Поляндий on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartThings_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
